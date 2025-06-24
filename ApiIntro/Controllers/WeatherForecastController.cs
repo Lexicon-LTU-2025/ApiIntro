ab00cd7926fd8991e7f16e4015f44ffb85877df3
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-24 13:46:05 +0200

    Some demo with WeatherController

--- ApiIntro/Controllers/WeatherForecastController.cs
+++ ApiIntro/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiIntro.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/weather")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -11,23 +11,28 @@ namespace ApiIntro.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController()
         {
-            _logger = logger;
+
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(int? number)
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = $"{Summaries[Random.Shared.Next(Summaries.Length)]} Number: {number}"
             })
             .ToArray();
+        }
+
+        [HttpGet("{banan}")]
+        public ActionResult<string> Get(string banan)
+        {
+            return Ok(banan);
         }
     }
 }
