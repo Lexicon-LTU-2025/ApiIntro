485f45c723376d1c507945ae04394cadd988606a
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-24 14:24:35 +0200

    Add SwaggerUI

--- ApiIntro/Program.cs
+++ ApiIntro/Program.cs
@@ -27,6 +27,11 @@ namespace ApiIntro
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+                });
             }
 
             app.UseHttpsRedirection();
