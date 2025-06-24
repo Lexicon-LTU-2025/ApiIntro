1031fc9b622799d6011ece888075b51f65868a98
DZM77 <dimitris.bjorlingh@outlook.com>
2025-06-24 13:52:56 +0200

    Add Employee

---
+++ ApiIntro/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiIntro.Models.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; } 
+    public int Salary { get; set; }
+
+}
